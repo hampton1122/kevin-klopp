e7f833fb46d16cfc9de70a319b689ea912a64c5f
Chris Hampton <hampton1122@gmail.com>
2018-01-07 20:18:04 -0500

    added scroll to the top element

--- js/main.js
+++ js/main.js
@@ -27,7 +27,21 @@ $(document).ready(function(){
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
 	});
-
+	
+	//scroll to top
+	$(window).scroll(function() {
+		if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+			$('#return-to-top').fadeIn(200);    // Fade in the arrow
+		} else {
+			$('#return-to-top').fadeOut(200);   // Else fade out the arrow
+		}
+	});
+	
+	$('#return-to-top').click(function() {      // When arrow is clicked
+		$('body,html').animate({
+			scrollTop : 0                       // Scroll to top of body
+		}, 500);
+	});
 
 	// google map
 		var map;
